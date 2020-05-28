4d9d04d9d218d7461b85a23bf11c74111162c629
rahma19 <rahmaamissaouii19@gmail.com>
2020-05-28 15:33:26 +0200

    firdt try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobordersComponent } from './src/joborders/joborders.component';
+import { WorkersComponent } from './workers/workers.component';
+import { AddOrdersComponent } from './add-orders/add-orders.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignOutComponent } from './sign-out/sign-out.component';
+import { SignContractComponent } from './sign-contract/sign-contract.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobordersComponent,
+    WorkersComponent,
+    AddOrdersComponent,
+    HomePageComponent,
+    SignInComponent,
+    SignOutComponent,
+    SignContractComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/joborders/joborders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joborders',
+  templateUrl: './joborders.component.html',
+  styleUrls: ['./joborders.component.css']
+})
+export class JobordersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
