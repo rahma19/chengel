5857c86b667bd24283ae827ec76e43117cd38e28
rahma19 <rahmaamissaouii19@gmail.com>
2020-05-29 03:21:42 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "homePage", component: HomePageComponent },
+  { path: "signContract", component: SignContractComponent },
+  { path: "singIn", component: SignInComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "addOrders", component: AddOrdersComponent },
+  { path: "jobOrders", component: JobordersComponent },
+  { path: "workers", component: WorkersComponent },
+  { path: "**", component: ErrorComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
