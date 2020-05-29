9071c20fc56a6c664ff06dd973e56340f42bf0b2
Yosrii <50800084+YosriZaghouani@Yosri.noreply.github.com>
2020-05-29 17:24:14 +0100

    29/05/2020 17:24 Yosri

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { JobordersComponent } from './src/joborders/joborders.component';
+import { JobordersComponent } from './joborders/joborders.component';
 import { WorkersComponent } from './workers/workers.component';
 import { AddOrdersComponent } from './add-orders/add-orders.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { SignOutComponent } from './sign-out/sign-out.component';
+/*import { SignOutComponent } from './sign-out/sign-out.component';*/
 import { SignContractComponent } from './sign-contract/sign-contract.component';
 import { LoginComponent } from './login/login.component';
 
@@ -20,7 +20,7 @@ import { LoginComponent } from './login/login.component';
     AddOrdersComponent,
     HomePageComponent,
     SignInComponent,
-    SignOutComponent,
+    /*SignOutComponent,*/
     SignContractComponent,
     LoginComponent
   ],
